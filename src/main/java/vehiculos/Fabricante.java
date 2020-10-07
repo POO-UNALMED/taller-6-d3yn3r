58d9995afe88cbb0efb651f92503726ed1a74222
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2020-10-06 23:00:23 -0500

    khjhkjk

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,27 +1,30 @@
 package vehiculos;
-import java.util.*;
 
-public class Fabricante  {
+import java.util.ArrayList;
+
+public class Fabricante {
 	public String nombre;
 	public Pais pais;
 	static ArrayList <Fabricante> fabrica=new ArrayList <Fabricante>();
 	
-	public Fabricante(String nombre,Pais pais) {
-		this.nombre=nombre;
-		this.pais=pais;
-	}
-	public Pais getPais() {
-		return pais;
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+		fabrica.add(this);
+		
 	}
 	
-	public void setPais(Pais pais) {
-		this.pais=pais;
-	}
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
 	}
 	
 	public static Fabricante fabricaMayorVentas() {
